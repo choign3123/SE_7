1f99b1c3967bb5ba683ca418015c6f7a738b7ec7
choign3123 <choign3123@naver.com>
2022-05-28 09:56:37 +0900

    Revert "파일 이름 잘못적어서 수정함"
    This reverts commit de819f7910b1801443d32a6f7bf9e985367c896c.

---
+++ server/se7-server-(손승현)/src/main/java/com/example/demo/src/clothes/ClthDao.java
@@ -0,0 +1,22 @@
+package com.example.demo.src.clothes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class ClthDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void test(){
+        System.out.println("test in clthDao");
+    }
+}
---
+++ server/se7-server/src/main/java/se7/closet/ClosetApplication.java
@@ -0,0 +1,19 @@
+package se7.closet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ClosetApplication.class, args);
+
+        // 메모리 사용량 출력
+        long heapSize = Runtime.getRuntime().totalMemory();
+        System.out.println("HEAP Size(M) : "+ heapSize / (1024*1024) + " MB");
+    }
+
+}
+
