5beaa69f32cd60746a71070bd26d61c6562b58f9
choign3123 <choign3123@naver.com>
2022-06-16 19:59:18 +0900

    서버 이미지 업로드 테스트코드

--- server/se7-server_ssh/src/main/java/se7/closet/src/test/TestController.java
+++ server/se7-server_ssh/src/main/java/se7/closet/src/test/TestController.java
@@ -10,14 +10,17 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import se7.closet.config.BaseResponse;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Date;
 
-@CrossOrigin(origins = "http://localhost:9000")
+@CrossOrigin(origins = {"http://localhost:9000"})
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -52,15 +55,19 @@ public class TestController {
         return "Success Test but no";
     }
 
+    //이미지 출력
+    // [GET] /test/display/{img}
     @ResponseBody
-    @GetMapping("display")
-    public ResponseEntity<Resource> showImg(@RequestParam("img") String imgName){
+    @GetMapping("display/{img}")
+    public ResponseEntity<Resource> showImg(@PathVariable("img") String imgName){
         System.out.println("이미지 출력 테스트");
 
+        //저장된 이미지의 경로
         String path = "C:\\Users\\Choi ga na\\Desktop\\SE_7\\server\\";
-        String folder = "";
+        String folder = "img\\";
+        //이미지 리소스
+        Resource resource = new FileSystemResource(path + folder + imgName);
 
-        Resource resource = new FileSystemResource(path + imgName);
         //이미지가 존재하지 않으면
         if(!resource.exists()){
             //HttpStatus.NOT_FOUND = 404 에러
@@ -68,15 +75,48 @@ public class TestController {
         }
 
         HttpHeaders headers = new HttpHeaders();
-        Path imgPath;
+        Path imgPath; //이미지 경로
 
         try{
+            //이미지 경로 설정
             imgPath = Paths.get(path + imgName);
+            //헤더에 이미지 확장자 넣기
             headers.add("Content-Type", Files.probeContentType(imgPath));
         } catch (IOException e){
-            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+    }
+
+    //이미지 저장(업로드)
+    // [POST] /test/upload
+    @ResponseBody
+    @PostMapping("upload")
+    public ResponseEntity<String> createImg(@RequestBody MultipartFile file){
+        // 시간과 originalFilename으로 매핑 시켜서 src 주소를 만들어 낸다.
+        Date date = new Date();
+        StringBuilder sb = new StringBuilder();
+
+        // file image 가 없을 경우
+        if (file.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        //이미지 이름 설정
+        sb.append(date.getTime());
+        sb.append(file.getOriginalFilename());
+        //이미지가 저장될 경로 설정
+        File dest = new File("C:\\Users\\Choi ga na\\Desktop\\SE_7\\server\\img\\" + sb.toString());
+
+        try {
+            file.transferTo(dest); //해당 경로로 이미지 옮기기
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<String>("성공", HttpStatus.OK);
     }
 }
