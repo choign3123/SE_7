12862da8f32765cdc56dc28558f6de5585f16aae
kyuseop-lim <xkfkrdlf@daum.net>
2022-06-21 22:01:23 +0900

    검색 화면.

--- front/MyClosetApp/app/src/main/java/com/example/myclosetapp/SearchActivity.kt
+++ front/MyClosetApp/app/src/main/java/com/example/myclosetapp/SearchActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myclosetapp.databinding.ActivitySearchBinding
+//import android.util.Log
 
 class SearchActivity : AppCompatActivity() {
 
@@ -20,59 +21,88 @@ class SearchActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         userIdx = intent.getIntExtra("userIdx", 0)
+        
+        var springSel : Boolean = false
+        var summerSel : Boolean = false
+        var fallSel : Boolean = false
+        var winterSel : Boolean = false
+        binding.buttonSpring.setOnClickListener(){springSel=true;binding.buttonSpring.isEnabled=false}
+        binding.buttonSummer.setOnClickListener(){summerSel=true;binding.buttonSummer.isEnabled=false}
+        binding.buttonFall.setOnClickListener(){fallSel=true;binding.buttonFall.isEnabled=false}
+        binding.buttonWinter.setOnClickListener(){winterSel=true;binding.buttonWinter.isEnabled=false}
 
-        binding.buttonSpring.setOnCheckedChangeListener { _, isChecked ->  }
-        binding.buttonSummer.setOnCheckedChangeListener { _, isChecked ->  }
-        binding.buttonFall.setOnCheckedChangeListener { _, isChecked ->  }
-        binding.buttonWinter.setOnCheckedChangeListener { _, isChecked ->  }
+        var btn9Sel : Boolean = false
+        var btn10Sel : Boolean = false
+        var btn11Sel : Boolean = false
+        var btn12Sel : Boolean = false
+        var btn13Sel : Boolean = false
+        var btn14Sel : Boolean = false
+        var btn15Sel : Boolean = false
+        var btn16Sel : Boolean = false
+        var btn17Sel : Boolean = false
+        var btn18Sel : Boolean = false
+        var btn19Sel : Boolean = false
+        var btn20Sel : Boolean = false
+        var btn21Sel : Boolean = false
+        var btn22Sel : Boolean = false
+        var btn23Sel : Boolean = false
+        var btn24Sel : Boolean = false
+        binding.button9.setOnClickListener(){btn9Sel=true;binding.button9.isEnabled=false}
+        binding.button10.setOnClickListener(){btn10Sel=true;binding.button10.isEnabled=false}
+        binding.button11.setOnClickListener(){btn11Sel=true;binding.button11.isEnabled=false}
+        binding.button12.setOnClickListener(){btn12Sel=true;binding.button12.isEnabled=false}
+        binding.button13.setOnClickListener(){btn13Sel=true;binding.button13.isEnabled=false}
+        binding.button14.setOnClickListener(){btn14Sel=true;binding.button14.isEnabled=false}
+        binding.button15.setOnClickListener(){btn15Sel=true;binding.button15.isEnabled=false}
+        binding.button16.setOnClickListener(){btn16Sel=true;binding.button16.isEnabled=false}
+        binding.button17.setOnClickListener(){btn17Sel=true;binding.button17.isEnabled=false}
+        binding.button18.setOnClickListener(){btn18Sel=true;binding.button18.isEnabled=false}
+        binding.button19.setOnClickListener(){btn19Sel=true;binding.button19.isEnabled=false}
+        binding.button20.setOnClickListener(){btn20Sel=true;binding.button20.isEnabled=false}
+        binding.button21.setOnClickListener(){btn21Sel=true;binding.button21.isEnabled=false}
+        binding.button22.setOnClickListener(){btn22Sel=true;binding.button22.isEnabled=false}
+        binding.button23.setOnClickListener(){btn23Sel=true;binding.button23.isEnabled=false}
+        binding.button24.setOnClickListener(){btn24Sel=true;binding.button24.isEnabled=false}
 
-        binding.button9.setOnCheckedChangeListener { _, isChecked ->  }
-        binding.button10.setOnCheckedChangeListener { _, isChecked ->  }
-        binding.button11.setOnCheckedChangeListener { _, isChecked ->  }
-        binding.button12.setOnCheckedChangeListener { _, isChecked ->  }
-        binding.button13.setOnCheckedChangeListener { _, isChecked ->  }
-        binding.button14.setOnCheckedChangeListener { _, isChecked ->  }
-        binding.button15.setOnCheckedChangeListener { _, isChecked ->  }
-        binding.button16.setOnClickListener(){binding.button16.isEnabled=false}
-        binding.button17.setOnClickListener(){binding.button17.isEnabled=false}
-        binding.button18.setOnClickListener(){binding.button18.isEnabled=false}
-        binding.button19.setOnClickListener(){binding.button19.isEnabled=false}
-        binding.button20.setOnClickListener(){binding.button20.isEnabled=false}
-        binding.button21.setOnClickListener(){binding.button21.isEnabled=false}
-        binding.button22.setOnClickListener(){binding.button22.isEnabled=false}
-        binding.button23.setOnClickListener(){binding.button23.isEnabled=false}
-        binding.button24.setOnClickListener(){binding.button24.isEnabled=false}
+        binding.switchBM.setOnCheckedChangeListener{_,isChecked -> }
 
-        binding.switchBM.setOnCheckedChangeListener { _, isChecked ->  }
-
-        binding.buttonGo.setOnClickListener() {
+        binding.buttonGo.setOnClickListener(){
             // 초기화 // 검색결과에서 다시 검색 시
-            myFav= null
+            myFav = null
             myCategory = " "
             mySeason = " "
 
             // season
-            if(binding.buttonSpring.isChecked == true) mySeason = mySeason +" "+binding.buttonSpring.textOn
-            if(binding.buttonSummer.isChecked == true) mySeason = mySeason +" "+binding.buttonSummer.textOn
-            if(binding.buttonFall.isChecked == true) mySeason = mySeason +" "+binding.buttonFall.textOn
-            if(binding.buttonWinter.isChecked == true) mySeason = mySeason +" "+binding.buttonWinter.textOn
+            if(springSel) mySeason = mySeason + " " + binding.buttonSpring.text
+            if(summerSel) mySeason = mySeason + " " + binding.buttonSummer.text
+            if(fallSel) mySeason = mySeason + " " + binding.buttonFall.text
+            if(winterSel) mySeason = mySeason + " " + binding.buttonWinter.text
 
             // category
-            if(binding.button9.isChecked == true) myCategory = myCategory +" "+binding.button9.textOn
-            if(binding.button10.isChecked == true) myCategory = myCategory +" "+binding.button10.textOn
-            if(binding.button11.isChecked == true) myCategory = myCategory +" "+binding.button11.textOn
-            if(binding.button12.isChecked == true) myCategory = myCategory +" "+binding.button12.textOn
-            if(binding.button13.isChecked == true) myCategory = myCategory +" "+binding.button13.textOn
-            if(binding.button14.isChecked == true) myCategory = myCategory +" "+binding.button14.textOn
-            if(binding.button15.isChecked == true) myCategory = myCategory +" "+binding.button15.textOn
+            if(btn9Sel) myCategory = myCategory + " " + binding.button9.text
+            if(btn10Sel) myCategory = myCategory + " " + binding.button10.text
+            if(btn11Sel) myCategory = myCategory + " " + binding.button11.text
+            if(btn12Sel) myCategory = myCategory + " " + binding.button12.text
+            if(btn13Sel) myCategory = myCategory + " " + binding.button13.text
+            if(btn14Sel) myCategory = myCategory + " " + binding.button14.text
+            if(btn15Sel) myCategory = myCategory + " " + binding.button15.text
+            if(btn16Sel) myCategory = myCategory + " " + binding.button16.text
+            if(btn17Sel) myCategory = myCategory + " " + binding.button17.text
+            if(btn18Sel) myCategory = myCategory + " " + binding.button18.text
+            if(btn19Sel) myCategory = myCategory + " " + binding.button19.text
+            if(btn20Sel) myCategory = myCategory + " " + binding.button20.text
+            if(btn21Sel) myCategory = myCategory + " " + binding.button21.text
+            if(btn22Sel) myCategory = myCategory + " " + binding.button22.text
+            if(btn23Sel) myCategory = myCategory + " " + binding.button23.text
+            if(btn24Sel) myCategory = myCategory + " " + binding.button24.text
 
             // favorite
-            if(binding.switchBM.isChecked == true) myFav =true
+            if(binding.switchBM.isChecked == true) myFav = true
             else myFav = false
 
             // 선택이 없다면 공백 보내기 선택이 있다면 공백 지우기
             if(mySeason != " ") mySeason = mySeason!!.trim()
-            if(myCategory != " ") myCategory =myCategory!!.trim()
+            if(myCategory != " ") myCategory = myCategory!!.trim()
 
             val intent = Intent(this@SearchActivity, SearchResultActivity::class.java)
             intent.putExtra("userIdx", userIdx)
@@ -82,9 +112,5 @@ class SearchActivity : AppCompatActivity() {
 
             startActivity(intent)
         }
-
     }
-
-
-
 }
